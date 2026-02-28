44cc3d7f35118e507e16e98392288048acc0f29f
Neethu C <sinetsoftware2025@gmail.com>
2026-02-28 16:25:53 +0530

    Dashboard fully responsive optimized

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -45,7 +45,7 @@ const Dashboard = () => {
   ];
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8 py-6 bg-gray-50 min-h-screen">
+    <div className="px-4 sm:px-6 lg:px-8 w-full ">
 
       {/* Header */}
       <div className="mb-6">
