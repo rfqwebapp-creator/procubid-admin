41b71047e8c20e020a41caef56af5c671324873e
Neethu C <sinetsoftware2025@gmail.com>
2026-03-26 13:05:18 +0530

    proxy fix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "http://3.110.143.13:5000/api",
+  // baseURL: "http://3.110.143.13:5000/api",
+  baseURL: "/api",
 });
 
 API.interceptors.request.use((req) => {
