5960cb64cc555565ae4fb1df8a237cf1714906c8
Neethu C <sinetsoftware2025@gmail.com>
2026-04-01 15:35:44 +0530

    fix api baseURL

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       //     password: password
       //   }
       // );
-const res = await API.post("/auth/login", {
+const res = await API.post("/login", {
   email,
   password
 });
