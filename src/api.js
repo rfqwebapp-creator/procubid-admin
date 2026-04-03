eb74f18909844f73fb5b7fd1b7d6d688c0a66efa
Neethu C <sinetsoftware2025@gmail.com>
2026-04-03 14:33:36 +0530

    updated API URL

--- src/api.js
+++ src/api.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
   // // baseURL: "http://3.110.143.13:5000/api",
-  baseURL: "/api",
+  // baseURL: "/api",
   // baseURL: "http://13.201.63.42:5000/api",
+
+  baseURL: "https://www.procubid.com/admin/api",
 });
 
 API.interceptors.request.use((req) => {
